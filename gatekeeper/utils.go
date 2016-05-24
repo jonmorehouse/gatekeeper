6c754712ea0123805f174b1efe84d4626b124a48
Jon Morehouse <morehousej09@gmail.com>
2016-05-23 23:48:40 -0700

    gatekeeper: upstream broadcaster / upstream-requester
    Adding in scaffolding around the upstream event stream. Specifically,
    this PR introduces the code around storing upstreams in a queryable
    place and exposing them to a potential server.

--- gatekeeper/utils.go
+++ gatekeeper/utils.go
@@ -2,10 +2,40 @@ package gatekeeper
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 )
+
+func RetryAndPanic(f func() error, retries uint) {
+	// retry a function n times before panicing and closing out the
+	// program. This should only be for exceptional cases
+	err := f()
+
+	for i := uint(0); i <= retries; i++ {
+		if err == nil {
+			return
+		}
+
+		err = f()
+	}
+	log.Fatal(err)
+}
+
+func NewUUID() (string, error) {
+	f, err := os.Open("/dev/urandom")
+	defer f.Close()
+	if err != nil {
+		return "", err
+	}
+
+	b := make([]byte, 16)
+	f.Read(b)
+
+	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]), nil
+}
 
 func ReqPrefix(req *http.Request) string {
 	// normalize the requestPath to end with a "/" if it doesn't already
