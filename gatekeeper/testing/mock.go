1bf2319e899bf0ddb0c2dd78a564841703e442f9
Jon Morehouse <morehousej09@gmail.com>
2016-08-17 22:41:51 -0700

    gatekeeper/testing: testing package
    Introduce a testing package which introduces a base mock type which can
    be used for building and asserting against mocks locally.
    
    Future iterations of this type will build out base mocks that can be
    used for other tests and such. For instance, a MockServiceContainer will
    be created if needed.
    
    Introduce assertions that can be used throughout the codebase.

---
+++ gatekeeper/testing/mock.go
@@ -0,0 +1,96 @@
+package testing
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type Mock interface {
+	SetCallSideEffect(method string, cb func(...interface{}))
+	SetCallResp(method string, resp ...interface{})
+
+	GetCalls(method string) [][]interface{}
+
+	AssertCalledWith(method string, args ...interface{})
+	AssertCalled(method string)
+	AssertCallCount(method string, count int)
+}
+
+func NewMock(t *testing.T) Mock {
+	return &mock{
+		t:     t,
+		calls: make(map[string][][]interface{}),
+	}
+}
+
+type mock struct {
+	t *testing.T
+
+	calls       map[string][][]interface{}
+	resp        map[string][]interface{}
+	sideEffects map[string]func(...interface{})
+}
+
+func (m *mock) SetCallSideEffect(method string, cb func(...interface{})) {
+	m.sideEffects[method] = cb
+}
+
+func (m *mock) SetCallResp(method string, resp ...interface{}) {
+	m.resp[method] = resp
+}
+
+func (m *mock) GetCalls(method string) [][]interface{} {
+	calls, ok := m.calls[method]
+	AssertTrue(m.t, ok)
+	return calls
+}
+
+func (m *mock) AssertCalledWith(method string, args ...interface{}) {
+	calls, ok := m.calls[method]
+	AssertTrue(m.t, ok)
+	AssertTrue(m.t, len(calls) > 0)
+
+	// iterate through each call
+	for _, call := range calls {
+		if len(call) != len(args) {
+			continue
+		}
+
+		// for each call, walk through each argument and check it
+		// against the input args
+		equal := true
+		for idx, arg := range call {
+			if !reflect.DeepEqual(arg, args[idx]) {
+				equal = false
+				break
+			}
+		}
+
+		// if the entirety of the call matched the correct args return
+		// because the test passes
+		if equal {
+			return
+		}
+	}
+
+	fail(m.t, fmt.Sprintf("did not find call with args %v", args))
+}
+
+func (m *mock) AssertNotCalled(method string) {
+	calls, ok := m.calls[method]
+	AssertTrue(m.t, (ok && len(calls) == 0) || !ok)
+}
+
+func (m *mock) AssertCalled(method string) {
+	calls, ok := m.calls[method]
+	AssertTrue(m.t, ok)
+	AssertTrue(m.t, len(calls) > 0)
+}
+
+func (m *mock) AssertCallCount(method string, count int) {
+	calls, ok := m.calls[method]
+
+	AssertTrue(m.t, ok)
+	AssertEqual(m.t, count, len(calls))
+}
